166fe54067060588ebfd17bd4a0371edfb8e1f55
PrathameshGandule <prathameshgandule1@gmail.com>
2025-04-05 14:17:50 +0530

    minor change

--- models/subjectmodel.js
+++ models/subjectmodel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const subjectSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    modules: [{ type: mongoose.Schema.Types.ObjectId, ref: "Module" }]
+    modules: [{ type: mongoose.Schema.Types.ObjectId, ref: "Module" }],
 }, { timestamps: true });
 
-module.exports = mongoose.model("Module", subjectSchema);
+module.exports = mongoose.model("Subject", subjectSchema);
