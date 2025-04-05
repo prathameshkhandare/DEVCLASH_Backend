ce449cfbc1b111ad24ab2d3cb55099a5c42d0a71
prathmesh_khandare <prathameshkhandare15@gmail.com>
2025-04-05 22:38:31 +0530

    done with test completed

--- controller/Profilecontroller.js
+++ controller/Profilecontroller.js
@@ -41,15 +41,51 @@ exports.testCompleted = async (req, res) => {
   try {
     const userId = req.user._id;
     const { testId, testScore } = req.body;
-    const updatedProfile = await Profile.findOneAndUpdate(
-      { _id: userId },
-      { $push: { completedTests: { testId, testScore } },
-        $inc: { totalScore: testScore } },
-      { new: true }
-    );
-    res.status(200).json({ message: "Test completed successfully", profile: updatedProfile });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
+
+    // Find profile
+    let profile = await Profile.findOne({ userId });
+
+    if (profile) {
+      // Check if test already exists
+      const testIndex = profile.completedTests.findIndex(
+        (test) => test.testId.toString() === testId
+      );
+
+      if (testIndex !== -1) {
+        const existingScore = profile.completedTests[testIndex].testScore;
+
+        if (testScore > existingScore) {
+          // Update score
+          profile.totalScore += testScore - existingScore;
+          profile.completedTests[testIndex].testScore = testScore;
+          await profile.save();
+        } // else do nothing
+      } else {
+        // New test entry
+        profile.completedTests.push({ testId, testScore });
+        profile.totalScore += testScore;
+        await profile.save();
+      }
+    } else {
+      // Create new profile if not exists
+      profile = await Profile.create({
+        userId,
+        completedTests: [{ testId, testScore }],
+        totalScore: testScore
+      });
+    }
+
+    res.status(200).json({
+      message: "Test processed successfully",
+      profile
+    });
+
+  } catch (error) {
+    console.error("Error in testCompleted:", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
   }
-}
+};
+
