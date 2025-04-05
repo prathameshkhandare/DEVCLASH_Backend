777dec9f89547f1f6e7d21f870f46ddd3a090d24
prathmesh_khandare <prathameshkhandare15@gmail.com>
2025-04-05 17:42:34 +0530

    sending whole user in resp

--- controller/Authcontroller.js
+++ controller/Authcontroller.js
@@ -71,7 +71,8 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     // Find user
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("-password");
+
     if (!user) return res.status(404).json({ message: "User not found" });
 
     // Compare passwords
@@ -84,13 +85,9 @@ const login = async (req, res) => {
       expiresIn: "1d",
     });
 
-    res.json({
+    res.status(200).json({
       message: "Login successful",
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-      },
+      user,
       token,
     });
   } catch (err) {
